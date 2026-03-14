afea99cbc59f5a8705c08c107ce73b3d59b11ac3
Tariqul911 <zxtariqul911@gmail.com>
2026-03-14 22:13:56 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Countries from "./components/Countries";
+
+function App() {
+  return (
+    <>
+      <h2>React world on the go....</h2>
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
